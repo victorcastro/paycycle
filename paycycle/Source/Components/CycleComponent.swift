b82bcfb28bc978e4834cc6df763a0a3321cfc698
Victor Castro <vhcastro@indracompany.com>
2022-12-22 12:46:01 -0500

    Add indicator to progress bar cycle

--- paycycle/Source/Components/CycleComponent.swift
+++ paycycle/Source/Components/CycleComponent.swift
@@ -12,6 +12,9 @@ struct CycleModel: Hashable {
     let dateStart: Date
     let dateFinish: Date
     let datePay: Date
+    let percentageTotal: Double
+    let percentageCycle: Double
+    let percentagePay: Double
 }
 
 struct CycleComponent: View {
@@ -28,24 +31,37 @@ struct CycleComponent: View {
         let currentMonth = Date().formatted(.dateTime.month(.twoDigits))
         let currentDay = Date().formatted(.dateTime.day())
         
-        let datePay = try? Date("\(cycle.dayPay)-\(Int(currentMonth)! + 1)-\(currentYear)", strategy: parseStrategy)
-        let dateFinish = try? Date("\(cycle.dayFinish)-\(currentMonth)-\(currentYear)", strategy: parseStrategy)
         let dateStart = try? Date("\(cycle.dayStart)-\(Int(currentMonth)! - 1)-\(currentYear)", strategy: parseStrategy)
+        let dateFinish = try? Date("\(cycle.dayFinish)-\(currentMonth)-\(currentYear)", strategy: parseStrategy)
+        let datePay = try? Date("\(cycle.dayPay)-\(Int(currentMonth)! + 1)-\(currentYear)", strategy: parseStrategy)
         
-        if (Int(currentDay)! > cycle.dayPay && Date.now > dateFinish!) {
-            let datePay2 = try? Date("\(cycle.dayPay)-\(Int(currentMonth)! + 2)-\(currentYear)", strategy: parseStrategy)
-            let dateFinish2 = try? Date("\(cycle.dayFinish)-\(Int(currentMonth)! + 1)-\(currentYear)", strategy: parseStrategy)
-            let dateStart2 = try? Date("\(cycle.dayStart)-\(currentMonth)-\(currentYear)", strategy: parseStrategy)
+        if let dateStart = dateStart, let dateFinish = dateFinish, let datePay = datePay {
+            let timeCycle = Calendar.current.dateComponents([.day], from: dateStart, to: dateFinish)
+            let timeCycleToNow = Calendar.current.dateComponents([.day], from: dateStart, to: Date.now)
             
+            let timeCycleToPay = Calendar.current.dateComponents([.day], from: dateFinish, to: datePay)
+            let timeCycleToPayNow = Calendar.current.dateComponents([.day], from: dateFinish, to: Date.now)
+            
+            print(timeCycle.day, timeCycleToNow.day, timeCycleToPay.day, timeCycleToPayNow.day)
+            
+            if (Int(currentDay)! > cycle.dayPay && Date.now > dateFinish) {
+                let datePay2 = try? Date("\(cycle.dayPay)-\(Int(currentMonth)! + 2)-\(currentYear)", strategy: parseStrategy)
+                let dateFinish2 = try? Date("\(cycle.dayFinish)-\(Int(currentMonth)! + 1)-\(currentYear)", strategy: parseStrategy)
+                let dateStart2 = try? Date("\(cycle.dayStart)-\(currentMonth)-\(currentYear)", strategy: parseStrategy)
+                
+                return [
+                    CycleModel(label: cycle.label!, dateStart: dateStart, dateFinish: dateFinish, datePay: datePay, percentageTotal: 0.7, percentageCycle: 0.60, percentagePay: 0),
+                    CycleModel(label: cycle.label!, dateStart: dateStart2!, dateFinish: dateFinish2!, datePay: datePay2!, percentageTotal: 0.9, percentageCycle: 1, percentagePay: 0.60)
+                ]
+            }
+                
             return [
-                CycleModel(label: cycle.label!, dateStart: dateStart!, dateFinish: dateFinish!, datePay: datePay!),
-                CycleModel(label: cycle.label!, dateStart: dateStart2!, dateFinish: dateFinish2!, datePay: datePay2!)
+                CycleModel(label: cycle.label!, dateStart: dateStart, dateFinish: dateFinish, datePay: datePay, percentageTotal: 0.4, percentageCycle: 1, percentagePay: 0.53)
             ]
         }
         
-        return [
-            CycleModel(label: cycle.label!, dateStart: dateStart!, dateFinish: dateFinish!, datePay: datePay!)
-        ]
+        return []
+        
     }
     
     private func labelSubCycle(cycle: CycleModel) -> Text {
@@ -78,22 +94,24 @@ struct CycleComponent: View {
                             Text("Pago")
                         }
                         
-                        Section() {
+                        ZStack() {
                             GeometryReader { geometry in
                                 HStack(spacing: 0.0) {
                                     Section() {
                                         GeometryReader { g1 in
-                                            Rectangle().fill(.green).frame(width: g1.size.width * 0.90)
+                                            Rectangle().fill(.green).frame(width: g1.size.width * subcycle.percentageCycle)
                                         }
                                     }
                                     Section() {
                                         GeometryReader { g2 in
-                                            Rectangle().fill(.red).frame(width: g2.size.width * 0.0)
+                                            Rectangle().fill(.blue).frame(width: g2.size.width * subcycle.percentagePay)
                                         }
                                     }
                                 }
                                 .frame(width: geometry.size.width)
                                 .background(.gray)
+                                Image(systemName: "arrowtriangle.up.fill").foregroundColor(Color.yellow).imageScale(.small)
+                                    .position(x: geometry.size.width * subcycle.percentageTotal, y: 13)
                             }
                         }.frame(height: 8)
                     }
